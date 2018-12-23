9fc5be6db96dc862df2b614db902cdc1bb6d3280
Hay Mar San <@haymarsan11@gmail.com>
2018-12-23 10:55:43 +0630

    test today

---
+++ app/src/main/java/com/kbz/hms/testtoday/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kbz.hms.testtoday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"testToday",Toast.LENGTH_SHORT).show();
+    }
+}
